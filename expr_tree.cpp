fbb111edb3ebba1918c2190d5f7bd2a88c23111d
leon2401119 <66659194+leon2401119@users.noreply.github.com>
2020-12-13 01:03:02 +0800

    1st working prototype

--- expr_tree.cpp
+++ expr_tree.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
+#include <stack>
 using namespace std;
-
 
 class expr_node{
 	public:
-		expr_node();
-		~expr_node();
-		virtual int eval();
+		virtual ~expr_node(){}
+		virtual int eval(){}
 };
 
-class int_node: public expr_node{
+class int_node: expr_node{
 	private:
 		int value;
 	public:
 		int_node(int num){
 			value = num;
 		}
-		~int_node(){}
+		~int_node(){
+		}
 		int eval(){
 			return value;
 		}
 };
 
-class binary_node: public expr_node{
+class binary_node: expr_node{
 	private:
 		string op;
 		expr_node* lchild;
@@ -56,11 +56,14 @@ class binary_node: public expr_node{
 };
 
 int main(){
-	/*
-	expr_node *exp = &binary_node("+",&int_node(1),&int_node(2));
-	*/
+    /*
 	int_node exp1 = int_node(1);
 	int_node exp2 = int_node(2);
-	binary_node exp3 = binary_node("+", &exp1, &exp2);
+	binary_node exp3 = binary_node("+", (expr_node*)&exp1, (expr_node*)&exp2);
+	*/
+	/*
+	// compile error "taking addr of temp"
+	binary_node exp3 = binary_node("+", (expr_node*)&int_node(1), (expr_node*)&int_node(2));
+	*/
 	cout << exp3.eval();
 }
