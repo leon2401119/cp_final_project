a87e2be7e3909056f717d61ef4c3e555e9ecd7c6
leon2401119 <66659194+leon2401119@users.noreply.github.com>
2020-12-16 12:08:15 +0800

    bug fixed (extra constructor for expr/binary_node)

--- expr.cpp
+++ expr.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 
+/*expr::expr(){
+    // another way to solve the bug is to new a meaningless node for p to point to whenever an expr is being created
+    // by calling its default constructor (either intentionally or accidentally)
+    // this way p->use will not cause seg fault
+
+    p = new int_node(0);
+}*/
+
 expr::expr(int num){
 	p = new int_node(num);
 }
@@ -12,13 +20,13 @@ expr::expr(char op, expr &l, expr &r){
 }
 
 int expr::eval(){
-	return p->eval();	
+	return p->eval();
 }
 
 
 expr& expr::operator=(const expr& rhs){
-	rhs.p->use++;
-	if(--(p->use))
+	(rhs.p->use)++;
+	if(!(--(p->use)))
 		delete p;
 	p = rhs.p;
 	return *this;
@@ -33,10 +41,25 @@ int int_node::eval(){
     return value;
 }
 
-binary_node::binary_node(char o, expr &lch, expr &rch){
+binary_node::binary_node(char o, expr &lch, expr &rch):op(o),lchild(lch),rchild(rch){
+
+
+    /*
+        triggers segmentation fault if binary node is initialized this way
+        supposed reason is that an expr object lchild is created first with default constructor
+        which does not exist beforehand. So the expr_node* p is
+        some random address pointing to some not allocated memory segment
+        And when lchild=lch is executed, by the definition of copy constructor
+        it will attempt to check p->use to see if the expr_node p was pointing to has another ref to it
+        Since the pointed memory by p is never allocated, p->use triggers segmentation fault
+    */
+
+    /*
     op = o;
     lchild = lch;
     rchild = rch;
+    */
+
 }
 
 binary_node::~binary_node(){
